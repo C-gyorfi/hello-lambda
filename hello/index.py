d1ddf812367faff9a438260d4fb212adee354528
csabagyorfi <csaba@madetech.com>
2022-01-03 16:44:21 +0000

    Use query parameter in lambda

--- hello/index.py
+++ hello/index.py
@@ -1,5 +1,11 @@
 def lambda_handler(event, context):
-    return {
-        'statusCode': '200',
-        'body': 'ho-ho-ho'
-    }
+    if event.get('queryStringParameters') and event['queryStringParameters'].get('name'):
+        return {
+            'statusCode': 200,
+            'body': 'Hello, ' + event['queryStringParameters']['name'] + '!'
+        }
+    else:
+        return {
+            'statusCode': 200,
+            'body': 'ho-ho-ho'
+        }

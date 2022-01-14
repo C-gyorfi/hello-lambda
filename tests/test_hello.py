cff5ffdecf6d7928ac50ed347d57883bc7bb72ea
csabagyorfi <csaba@madetech.com>
2022-01-14 16:51:40 +0000

    Use subprocess to setup credentials for API test

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -2,17 +2,10 @@ from requests.models import Response
 from hello import __version__
 import os
 import requests
+import subprocess
 
-def send_api_request():
-    base_url = os.getenv('BASE_URL').strip('"')
-    auth_token = "Bearer " + os.getenv('API_KEY')
-    return requests.get(
-        f'{base_url}/hello',
-        headers={
-            "Authorization": auth_token,
-            "Content-Type": "application/json",
-        }
-    )
+def setup_module(module):
+    setup_credentials()
 
 def test_version():
     assert __version__ == '0.1.0'
@@ -22,3 +15,18 @@ def test_api_response_200():
 
 def test_api_response_message():
     assert send_api_request().content == b'{"message": "Hello, World!"}'
+
+def send_api_request():
+    base_url = os.getenv('BASE_URL')
+    auth_token = "Bearer " + os.getenv('HELLO_API_KEY')
+    return requests.get(
+        f'{base_url}/hello',
+        headers={
+            "Authorization": auth_token,
+            "Content-Type": "application/json",
+        }
+    )
+
+def setup_credentials():
+    os.environ['HELLO_API_KEY'] = subprocess.check_output('./set_api_key.sh', shell=True).decode().strip()
+    os.environ['BASE_URL'] = subprocess.check_output('terraform output -raw base_url', shell=True).decode().strip()
